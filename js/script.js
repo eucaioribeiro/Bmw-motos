5fed7f0c9155e6734b57850cefc82104f76f5092
Caio Ribeiro <caioscarpim@hotmail.com>
2022-07-07 16:30:40 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let btnVermelho = document.getElementById('vermelho');
+let btnAzul = document.getElementById('azul');
+let btnPreto = document.getElementById('preto');
+let moto = document.getElementById('moto');
+
+btnVermelho.onclick = function(){
+    moto.style.backgroundImage = "url(/img/BMW1.png";
+}
+
+btnAzul.onclick = function(){
+    moto.style.backgroundImage = "url(/img/BMW2.png)";
+}
+
+btnPreto.onclick = function(){
+    moto.style.backgroundImage = "url(/img/BMW3.png)";
+}
